70c9204efc504569c0200f6e53716c610b257e01
xcssum <xcssummer@gmail.com>
2017-03-02 22:49:14 +0800

    init

---
+++ WxBot.ts
@@ -0,0 +1,8 @@
+import Wechaty from 'wechaty';
+
+const bot = Wechaty.instance();
+// bot.on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
+// .on('login',user => console.log(`User ${user} logined`))
+// .on('message',  message => console.log(`Message: ${message}`))
+// bot.init();
+console.log("test")
