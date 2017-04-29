16a20160387a856138c8be1278946cdee4b17b10
xcssum <xcssummer@gmail.com>
2017-04-29 09:24:06 +0800

    完成第一次反馈

--- wx_bot.js
+++ wx_bot.js
@@ -4,5 +4,9 @@ console.log(wechaty)
 const bot = wechaty.instance();
 bot.on('scan', function(url, code) { console.log(" Scan QR Code to login: " + code + "\n" + url)})
 .on('login',function(user) { console.log("user " + user + " login success")})
-.on('message',  function(message) { console.log("Message: " + message)})
+.on('message',function(message) { if(!message.self()){
+    if(message.from() === "PlanX"){
+        message.say("success")
+    }
+} })
 bot.init();
